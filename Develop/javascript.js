8c9f30e69703ec36345b1b10f8341855bd7a6fe1
Vani Karun <vanikarun21@yahoo.com>
2022-04-07 20:33:27 -0500

    updated js and css files with date and daily planner codes

---
+++ Develop/javascript.js
@@ -0,0 +1,10 @@
+
+
+var dt = new Date();
+var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+document.write(time);
+
+new Date(Date.now()) 
+new Date($.now());
+
+// $( "#datepicker" ).datepicker({dateFormat:"yy/mm/dd"}).datepicker("setDate",new Date()); 
