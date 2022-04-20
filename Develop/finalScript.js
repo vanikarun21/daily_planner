cf1568206c3a239da6baf5a3b47564e17b963279
Vani Karun <vanikarun21@yahoo.com>
2022-04-19 19:24:22 -0500

    updated js file with localstorage and color codes

--- Develop/finalScript.js
+++ Develop/finalScript.js
@@ -3,60 +3,161 @@
 function getHeaderDate() {
     var currentHeaderDate = moment().format('dddd, MMMM Do h A');
     $("#currentDay").text(currentHeaderDate);
+    
 }
 
 getHeaderDate();
 
+
+var dataTemplate = {"9":"", "10":"","11":"","12":"","13":"","14":"","15":"","16":"","17":"",} // creating objects
+// localStorage.setItem('textinput',JSON.stringify(dataTemplate))
+
+
 var timeContainer = $(".container")
 //  create a for loop to list 8 hrs 
 for (var i = 0; i < 9; i++) {
+    console.log(moment().hours());
+
+    var checkTime = 0
 
     let time = ""
     if (i === 0) {
         time = "9 am"
+        checkTime = 9
     } else if (i === 1) {
         time = "10 am"
-
+        checkTime = 10
     } else if (i === 2) {
         time = "11 am"
-
+        checkTime = 11
     } else if (i === 3) {
         time = "12 pm"
-
+        checkTime = 12
     } else if (i === 4) {
         time = "1 pm"
-
+        checkTime = 13
     } else if (i === 5) {
         time = "2 pm"
-
+        checkTime = 14
     } else if (i === 6) {
         time = "3 pm"
-
+        checkTime = 15
     } else if (i === 7) {
         time = "4 pm"
-
+        checkTime = 16
     } else if (i === 8) {
         time = "5 pm"
-
+        checkTime = 17
     }
+    var currentTime = moment().hours()
+      var textColor = ""   // this variable is the dynamic class declaration for the text areas
+
+    if (currentTime > checkTime) {
+        textColor = "past"
+    } else if (currentTime === checkTime) {
+        textColor = "present"
+    } else if (currentTime <checkTime) {
+        textColor = "future"
+    }
+
     let hourBlock = $("<div class = 'row'></div>")
     //  create a var for p html element - $ blue color - template literal
     let num = $(`<p id="mainRow" class = 'hour col-2 justify content left'>${time}</p>`)
     // create a var for textarea html element
-    let txtArea = $("<textarea class = 'text col-8 d-flex justify content center'></textarea>")
+    let txtArea = $(`<textarea id="message"class = 'text col-8 d-flex justify content center'></textarea>`)
     // create a var for button 
-    let btn = $("<button class = 'saveBtn col-2'></button>")
-    // create a variable and then if statement i = 0, then 
-    let hourRow = $(`<div class = 'row'><p id="mainRow" class = 'hour col-2 justify content left'>${time}</p><textarea class = 'text col-8 d-flex justify content center'></textarea><button class = 'saveBtn col-2'></button></div>`)
+    let btn = $(`<button  id="save" class = 'saveBtn col-2' >Submit</button>`)
+    // create a variable and then if statement i = 0, then
+    var localStorageData = JSON.parse(localStorage.getItem('textinput')) || dataTemplate
+    let hourRow = $(`<div class = 'row'><p id="mainRow" class = 'hour col-2 justify content left'>${time}</p><textarea id="${checkTime}" class = '${textColor} text col-8 d-flex justify content center'>${localStorageData[checkTime]}</textarea><button id="save" class = 'saveBtn col-2'>Submit</button></div>`)
     // no jquery equivalent for the below so leave them as it is 
     // hourBlock.append(num,txtArea,btn)
     // timeContainer.append(hourBlock)
     timeContainer.append(hourRow)
-
+    
 };
 
-var data = 
-$("<textarea>").addClass("past");
+
+// var storageInput = document.querySelector('#message');
+var button = document.querySelectorAll('.saveBtn');  // use only the class you use and not 2 classes or bootstrap when  you use querySelector
+// var storedInput = localStorage.getItem('textinput');
+// if(storageInput) {
+//     message.textContent = storageInput
+for (let i=0 ; i < button.length; i++) {
+button[i].addEventListener('click', (event) => {
+    event.preventDefault()
+    // console.log (event)
+    // console.log(event.target)
+    // console.log(event.target.previousElementSibling)
+    console.log(event.target.previousElementSibling.value)
+    console.log(event.target.previousElementSibling.id)
+    var textAreaValue = event.target.previousElementSibling.value
+    var textAreaId = event.target.previousElementSibling.id
+    dataTemplate[textAreaId] = textAreaValue
+    localStorage.setItem('textinput',JSON.stringify(dataTemplate))
+})
+}
+
+// var saveToLocalStorage = () => {
+//     localStorage.setItem('textinput', message.textContent)
+// }
+
+// button.addEventListener('click', saveToLocalStorage)
+
+// =============================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let btn = $(`<button onclick="fn1()" id="save" class = 'saveBtn col-2' >Submit</button>`)
+
+// let txtArea = $(`<textarea id="message"class = 'text col-8 d-flex justify content center'></textarea>`)
+
+// let hourRow = $(`<div class = 'row'><p id="mainRow" class = 'hour col-2 justify content left'>${time}</p><textarea id="message" class = 'text col-8 d-flex justify content center'></textarea><button onclick="fn1" id="save" class = 'saveBtn col-2'>Submit</button></div>`)
+
+// function fn1(){
+// var getData = document.getElementById("message").value;
+// document.getElementById("message").innerHTML = getData;
+// }
+
+// fn1();
+
+// =================================================================================================================================
+
+
+
+
+
+// function store (){
+//     var toDoList = document.getElementsByTagName('textarea').value;
+// }
+
+// window.localStorage.setItem('type'. JSON.stringify(toDoList));
+
+// JSON.parse(window.localStorage.getItem('type'));
 
 
 
@@ -108,4 +209,3 @@ $("<textarea>").addClass("past");
     
 // }
 // write code to align the text you input pertains to the hour you added it to
-//  make sure the info we are storing in local storage is not detroying the old information
